2b18ef92d7104d49cd08f96fccf1b07edc34db04
Mikael Sundell <mikael.sundell@gmail.com>
2023-12-12 19:24:01 +0100

    build.sh improvements Moved freetype to build_base Updated OpenColorIO Updated MaterialX python path Updated OpenImageIO python path Fixed USD

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ machine_arch=$(uname -m)
 macos_version=$(sw_vers -productVersion)
 major_version=$(echo "$macos_version" | cut -d '.' -f 1)
 
+# exit on error
+set -e 
+
 clear
 echo "Building 3rdparty for $build_type"
 echo "---------------------------------"
@@ -20,11 +23,15 @@ fi
 
 # check if cmake is in the path
 if ! command -v cmake &> /dev/null; then
-    echo "cmake not found in the PATH. Please make sure it is installed and in the PATH."
-    exit 1
+    echo "cmake not found in the PATH, will try to set to /Applications/CMake.app/Contents/bin"
+    export PATH=$PATH:/Applications/CMake.app/Contents/bin
+    if ! command -v cmake &> /dev/null; then
+        echo "cmake could not be found, please make sure it's installed"
+        exit 1
+    fi
 fi
 
-# cbuild qt
+# build qt
 qt_name="qt-everywhere-src-6.6.0"
 qt_url="https://mikaelsundell.s3.eu-west-1.amazonaws.com/3rdparty/$qt_name.tar.gz"
 build_qt() {
@@ -57,37 +64,55 @@ build_3rdparty() {
     # cmake
     export PATH=$PATH:/Applications/CMake.app/Contents/bin &&
 
+    # set an alias for Python
+    alias python=/usr/bin/python3 &&
+
     # script dir
     cd "$script_dir"
 
     # make base
     echo "Build 3rdparty base for type: $build_type"
     make verbose=1 build_base=1 $build_type &&
+
+    echo "Build Qt for type: $build_type"
     cd "qt/$qt_name" &&
 
-    # remove the existing build directory and create a new one
-    rm -fR build.$build_type &&
-    mkdir build.$build_type &&
-    cd build.$build_type &&
+    # deployment target
+    export MACOSX_DEPLOYMENT_TARGET=$major_version &&
+    echo "Qt deployment target: $major_version"
+      
+    if [ -d "build.$build_type" ]; then
+        # reuse existing build
+        echo "Qt build for type: $build_type exists, will reuse"
+        cd build.$build_type
+    else
+        # create new build
+        mkdir build.$build_type &&
+        cd build.$build_type &&
 
-    # set an alias for Python
-    alias python=/usr/bin/python3 &&
-
-    # configure Qt with the appropriate options
-    echo "Build Qt for type: $build_type"
-    ../configure -prefix "$script_dir/build/macosx/$machine_arch.$build_type" -libdir "$script_dir/build/macosx/$machine_arch.$build_type/lib" -opensource -confirm-license -system-libpng -system-libjpeg -system-zlib -system-pcre -system-harfbuzz -skip qtactiveqt -skip qtcharts -skip qtconnectivity -skip qtlocation -skip qtsensors -skip qtspeech -skip qtvirtualkeyboard -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebview -I"$script_dir/build/macosx/$machine_arch.$build_type/include" -L"$script_dir/build/macosx/$machine_arch.$build_type/lib" $configure_options &&
+        # configure Qt with the appropriate options
+        echo "Build Qt for type: $build_type"
+        ../configure -prefix "$script_dir/build/macosx/$machine_arch.$build_type" -libdir "$script_dir/build/macosx/$machine_arch.$build_type/lib" -opensource -confirm-license -system-libpng -system-libjpeg -system-zlib -system-pcre -system-harfbuzz -system-freetype -skip qtactiveqt -skip qtcharts -skip qtconnectivity -skip qtlocation -skip qtsensors -skip qtspeech -skip qtvirtualkeyboard -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebview -I"$script_dir/build/macosx/$machine_arch.$build_type/include" -L"$script_dir/build/macosx/$machine_arch.$build_type/lib" $configure_options
+    fi
 
     # make install Qt
     make install &&
 
     # make extras
     echo "Build 3rdparty extras for type: $build_type"
-    cd "$script_dir" #&&
+    cd "$script_dir" &&
     make verbose=1 build_libs=1 build_extras=1 $build_type
+}
+
+build_clean() {
+    cd "$script_dir"
+    echo "Clean 3rdparty for all"
+    make clean
 }
 
 # build types
 if [ "$build_type" == "all" ]; then
+    build_clean
     build_3rdparty "debug"
     build_3rdparty "release"
 else
